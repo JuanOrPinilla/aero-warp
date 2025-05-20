1390498eb1b4efa915e6b7619b61f6c9b742046b
Juan David Orduz Pinilla <j.orduz@uniandes.edu.co>
2025-05-19 22:17:07 -0500

    nubes

--- src/create/prefab_creator_game.py
+++ src/create/prefab_creator_game.py
@@ -10,6 +10,9 @@ from src.ecs.components.c_transform import CTransform
 from src.ecs.components.tags.c_tag_player import  CTagPlayer
 from src.engine.service_locator import ServiceLocator
 
+import random
+
+
 def create_player(world: esper.World, player_cfg: dict, player_start_cfg: dict):
     surf = ServiceLocator.images_service.get(player_cfg["image"])
     width, height = surf.get_size()
@@ -22,7 +25,6 @@ def create_player(world: esper.World, player_cfg: dict, player_start_cfg: dict):
         (screen_width / 2) - (frame_width / 2),
         screen_height / 2
     )
-    print(pos)
 
     vel = pygame.Vector2(0, 0)
     player_ent = create_sprite(world, pos, vel, surf)
@@ -30,6 +32,61 @@ def create_player(world: esper.World, player_cfg: dict, player_start_cfg: dict):
     world.add_component(player_ent, CAnimation(player_cfg["animation"]))
     return player_ent
 
+def create_cloud_mediumA(world: esper.World, level_cfg: dict, player_start_cfg: dict):
+    surf = ServiceLocator.images_service.get("assets/img/clouds_medium_A.png")
+    screen_width = 224 * 4
+    screen_height = 256 * 4
+
+    x = random.uniform(0, screen_width)
+    y = random.uniform(0, screen_height)
+    pos = pygame.Vector2(x, y)
+
+    cloud_ent = create_sprite(world, pos, pygame.Vector2(0, 0), surf)
+    world.add_component(cloud_ent, CAnimation(level_cfg["animation"]))
+    return cloud_ent
+
+def create_cloud_mediumB(world: esper.World, level_cfg: dict, player_start_cfg: dict):
+    surf = ServiceLocator.images_service.get("assets/img/clouds_medium_B.png")
+    screen_width = 224 * 4
+    screen_height = 256 * 4
+
+    x = random.uniform(0, screen_width)
+    y = random.uniform(0, screen_height)
+    pos = pygame.Vector2(x, y)
+
+    cloud_ent = create_sprite(world, pos, pygame.Vector2(0, 0), surf)
+    world.add_component(cloud_ent, CAnimation(level_cfg["animation"]))
+    return cloud_ent
+
+def create_cloud_small(world: esper.World, level_cfg: dict, player_start_cfg: dict):
+    surf = ServiceLocator.images_service.get("assets/img/clouds_small.png")
+    screen_width = 224 * 4
+    screen_height = 256 * 4
+
+    x = random.uniform(0, screen_width)
+    y = random.uniform(0, screen_height)
+    pos = pygame.Vector2(x, y)
+
+    cloud_ent = create_sprite(world, pos, pygame.Vector2(0, 0), surf)
+    world.add_component(cloud_ent, CAnimation(level_cfg["animation"]))
+    return cloud_ent
+
+def create_cloud_large(world: esper.World, level_cfg: dict, player_start_cfg: dict):
+    surf = ServiceLocator.images_service.get("assets/img/clouds_large.png")
+    width, height = surf.get_size()
+    frame_width = width / 32
+
+    screen_width = 224 * 4
+    screen_height = 256 * 4
+
+    pos = pygame.Vector2(
+        (screen_width / 2) - (frame_width / 2),
+        screen_height / 2
+    )
+
+    cloud_ent = create_sprite(world, pos, None, surf)
+    world.add_component(cloud_ent, CAnimation(level_cfg["animation"]))
+    return cloud_ent
 
 def create_game_input(world:esper.World):
     quit_to_menu_action = world.create_entity()
--- src/game/play_scene.py
+++ src/game/play_scene.py
@@ -1,11 +1,13 @@
 import json
+import random
 import pygame
 
+from src.create.prefab_creator import create_sprite
 from src.ecs.components.c_animation import CAnimation, set_animation
 from src.ecs.components.tags.c_tag_player import CTagPlayer
 from src.ecs.systems.s_animation import get_animation_by_angle, system_animation
 from src.engine.scenes.scene import Scene
-from src.create.prefab_creator_game import create_player, create_game_input
+from src.create.prefab_creator_game import create_cloud_large, create_cloud_mediumA, create_cloud_mediumB, create_cloud_small, create_player, create_game_input
 from src.create.prefab_creator_interface import TextAlignment, create_text
 from src.ecs.components.c_input_command import CInputCommand, CommandPhase
 from src.ecs.components.c_surface import CSurface
@@ -54,7 +56,27 @@ class PlayScene(Scene):
         self._player_ent = player_ent
         self._p_v = self.ecs_world.component_for_entity(player_ent, CVelocity)
         self._p_t = self.ecs_world.component_for_entity(player_ent, CTransform)
-                
+        
+        self._cloud_ents = []
+        for _ in range(50):  # 5 nubes grandes
+            ent = create_cloud_large(self.ecs_world, self.level_cfg, self.level_cfg["player_start"])
+            self._cloud_ents.append(ent)
+            
+        for _ in range(50):  # 5 nubes medianas A
+            ent = create_cloud_mediumA(self.ecs_world, self.level_cfg, self.level_cfg["player_start"])
+            self._cloud_ents.append(ent)
+        
+        for _ in range(50):  # 5 nubes medianas B
+            ent = create_cloud_mediumB(self.ecs_world, self.level_cfg, self.level_cfg["player_start"])
+            self._cloud_ents.append(ent)
+        
+        for _ in range(50):  # 5 nubes pequeñas
+            ent = create_cloud_small(self.ecs_world, self.level_cfg, self.level_cfg["player_start"])
+            self._cloud_ents.append(ent)
+        
+        # Si quieres guardar los CTransform para cada nube, puedes hacer algo así:
+        self._cloud_transforms = [self.ecs_world.component_for_entity(e, CTransform) for e in self._cloud_ents]
+                    
         paused_text_ent = create_text(self.ecs_world, "PAUSE", 12, 
                                  pygame.Color(255, 50, 50), pygame.Vector2(112, 120), 
                                  TextAlignment.CENTER)
@@ -74,26 +96,37 @@ class PlayScene(Scene):
                 self.p_txt_s.visible = not self.p_txt_s.visible
                 self._pause_blink_timer = 0.0
             return
-        if not self._paused:
-            dir_x = (-1 if self._dir_keys["LEFT"] else 0) + (1 if self._dir_keys["RIGHT"] else 0)
-            dir_y = (-1 if self._dir_keys["UP"] else 0) + (1 if self._dir_keys["DOWN"] else 0)
-            new_dir = pygame.Vector2(dir_x, dir_y)
 
-            if new_dir.length_squared() > 0:
-                self._move_dir = new_dir.normalize()
+        # Capturar la dirección del movimiento según input
+        dir_x = (-1 if self._dir_keys["LEFT"] else 0) + (1 if self._dir_keys["RIGHT"] else 0)
+        dir_y = (-1 if self._dir_keys["UP"] else 0) + (1 if self._dir_keys["DOWN"] else 0)
+        new_dir = pygame.Vector2(dir_x, dir_y)
 
-                players = self.ecs_world.get_component(CTagPlayer)
-                for ent, _ in players:
-                    anim = self.ecs_world.component_for_entity(ent, CAnimation)
-                    anim_name = get_animation_by_angle(self._move_dir.x, self._move_dir.y)
-                    set_animation(anim, anim_name)
-                    break
+        if new_dir.length_squared() > 0:
+            self._move_dir = new_dir.normalize()
 
-            self._p_v.vel = self._move_dir * self._move_speed
+            # Actualizar animación del jugador según dirección
+            players = self.ecs_world.get_component(CTagPlayer)
+            for ent, _ in players:
+                anim = self.ecs_world.component_for_entity(ent, CAnimation)
+                anim_name = get_animation_by_angle(self._move_dir.x, self._move_dir.y)
+                set_animation(anim, anim_name)
+                break
 
-            system_screen_player(self.ecs_world, self.screen_rect)
-            system_movement(self.ecs_world, delta_time)
-            system_animation(self.ecs_world, delta_time)
+        self._p_v.vel = self._move_dir * self._move_speed
+
+        delta_pos = -self._p_v.vel * delta_time
+
+        # Mover nubes
+        for transform in self._cloud_transforms:
+            transform.pos += delta_pos
+
+        # Mover otros objetos del mundo 
+        system_screen_player(self.ecs_world, self.screen_rect)
+
+        # Actualizar animaciones normalmente
+        system_animation(self.ecs_world, delta_time)
+
 
 
     def do_clean(self):
@@ -107,6 +140,29 @@ class PlayScene(Scene):
                 surface.visible = visible
                 if not visible:
                     self._paused_entities.append(ent)
+
+    def _generate_clouds_around_player(self):
+        buffer_distance = 300 
+        current_pos = self._p_t.pos
+
+        for _ in range(5):  
+            offset_x = random.uniform(-buffer_distance, buffer_distance)
+            offset_y = random.uniform(-buffer_distance, buffer_distance)
+            new_pos = current_pos + pygame.Vector2(offset_x, offset_y)
+
+            if random.random() < 0.25:
+                ent = create_cloud_large(self.ecs_world, self.level_cfg, {"x": new_pos.x, "y": new_pos.y})
+            elif random.random() < 0.5:
+                ent = create_cloud_mediumA(self.ecs_world, self.level_cfg, {"x": new_pos.x, "y": new_pos.y})
+            elif random.random() < 0.75:
+                ent = create_cloud_mediumB(self.ecs_world, self.level_cfg, {"x": new_pos.x, "y": new_pos.y})
+            else:
+                ent = create_cloud_small(self.ecs_world, self.level_cfg, {"x": new_pos.x, "y": new_pos.y})
+            
+            transform = self.ecs_world.component_for_entity(ent, CTransform)
+            transform.pos = new_pos
+            self._cloud_ents.append(ent)
+            self._cloud_transforms.append(transform)
 
             
     def do_action(self, action: CInputCommand):
